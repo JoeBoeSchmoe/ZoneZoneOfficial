33cbd52eafa57ab9f994a02515c4776cccab4089
Joseph Sheets <JSheets1@IAState.edu>
2025-03-14 05:22:40 -0500

    added fixed project start

---
+++ backend/zonezone-backend/src/main/java/com/zonezone/backend/ZonezoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zonezone.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonezoneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZonezoneBackendApplication.class, args);
+    }
+
+}
---
+++ backend/zonezone-backend/src/test/java/com/zonezone/backend/ZonezoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zonezone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonezoneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
