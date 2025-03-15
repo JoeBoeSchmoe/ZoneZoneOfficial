1f0621082d1ea7590bf37bb9ecdfaf4ad9f0b33e
Joseph Sheets <JSheets1@IAState.edu>
2025-03-15 01:43:42 -0500

    established connection with new server

--- backend/zonezone-backend/src/main/java/com/zonezone/backend/accountHandler/accountRelatedModels/UserAccountModel.java
+++ backend/zonezone-backend/src/main/java/com/zonezone/backend/accountHandler/accountRelatedModels/UserAccountModel.java
@@ -5,12 +5,14 @@ package com.zonezone.backend.accountHandler.accountRelatedModels;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Entity
+@Table(name = "user_accounts") // Custom table name
 public class UserAccountModel {
 
     @Id
